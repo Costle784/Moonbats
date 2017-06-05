d7c52c88bec3dd0bf03f3ad7e010bbf5b629034a
Curtis Ostle <curtisostle@gmail.com>
2017-06-05 10:08:15 -0400

    making home view

---
+++ moonbats-react/src/components/App/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import SearchContainer from '../TeamPicker/TeamPicker.js'
+import Results from '../Results/Results.js'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom'
+import './App.css'
+
+
+
+
+
+
+
+
+export default App;
