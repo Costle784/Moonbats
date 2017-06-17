ddf4361effda54b782c82f482a7e0658c4f07844
Curtis Ostle <curtisostle@gmail.com>
2017-06-16 20:29:02 -0400

    turn off threadsafe

--- moonbats_rails_api/config/environments/production.rb
+++ moonbats_rails_api/config/environments/production.rb
@@ -7,8 +7,9 @@ Rails.application.configure do
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
+  # Rake tasks automatically ignore this option for perform`ance.
   config.eager_load = true
+  config.threadsafe = false
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
@@ -82,5 +83,5 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = true
+  config.active_record.dump_schema_after_migration = false
 end
